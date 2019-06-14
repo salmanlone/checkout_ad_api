eb185898960065c7d512b10d278a5581f73930b6
Salman Tariq Lone <salmanlone89@gmail.com>
2019-06-14 16:42:28 +0800

    integration of swagger UI for API endpoints.

--- CheckoutAd.Api/Startup.cs
+++ CheckoutAd.Api/Startup.cs
@@ -1,33 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace CheckoutAd.Api
 {
+	/// <summary>
+	/// Startup class of .netcore project.
+	/// </summary>
 	public class Startup
 	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="configuration"></param>
 		public Startup(IConfiguration configuration)
 		{
 			Configuration = configuration;
 		}
 
+		/// <summary>
+		/// Configuration property
+		/// </summary>
 		public IConfiguration Configuration { get; }
 
-		// This method gets called by the runtime. Use this method to add services to the container.
+		/// <summary>
+		/// This method gets called by the runtime. Use this method to add services to the container.
+		/// </summary>
+		/// <param name="services"></param>
 		public void ConfigureServices(IServiceCollection services)
 		{
 			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+				{
+					Version = "v1",
+					Title = "Checkout Ad Api Documentation",
+					Description = "This documentation provide the information about check out ad api endpoints.",
+					TermsOfService = "None",
+					Contact = new Swashbuckle.AspNetCore.Swagger.Contact()
+					{
+						Name = "Salman Tariq Lone",
+						Email = "salman@tutorial.com",
+						Url = ""
+					}
+				});
+			});
+			services.AddSwaggerGen(c =>
+			{
+				c.IncludeXmlComments(GetXmlCommentsPath());
+				c.DescribeAllEnumsAsStrings();
+			});
 		}
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		/// <summary>
+		/// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		/// </summary>
+		/// <param name="app"></param>
+		/// <param name="env"></param>
 		public void Configure(IApplicationBuilder app, IHostingEnvironment env)
 		{
 			if (env.IsDevelopment())
@@ -36,6 +70,16 @@ namespace CheckoutAd.Api
 			}
 
 			app.UseMvc();
+			app.UseSwagger();
+			app.UseSwaggerUI(c =>
+			{
+				c.SwaggerEndpoint("/swagger/v1/swagger.json", "Checkout Ad Api");
+			});
+		}
+
+		private string GetXmlCommentsPath()
+		{
+			return Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "CheckoutAd.Api.xml");
 		}
 	}
 }
